3c9db378f51c0940e470dd6d6dfe4aa07ef3ded1
Dylan Meacham <91638140+DylanTMeacham@users.noreply.github.com>
2023-01-20 18:00:52 -0500

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -32,29 +32,60 @@ public class Arm extends SubsystemBase {
     // This func will calculate what angle our arm needs to be when we drop the cone or cube
 
     public static double calculateArmDropAngle(boolean isTopRow, boolean isCone, double armHeight, double distanceFromGrid) {
-        double nodeX;
-        double nodeY;
-        double dropHeight;
 
-        if (isCone) {
-            dropHeight = Constants.Arm.dropHeightPeg; //dont have values yet
-            if (isTopRow) {
-                nodeX = Constants.Arm.TopX;
-                nodeY = Constants.Arm.TopPegY;
-            } else {
-                nodeX = Constants.Arm.MidX;
-                nodeY = Constants.Arm.MidPegY;
-            }
-        } else {
-            dropHeight = Constants.Arm.dropHeightShelf; //dont have values yet
-            if (isTopRow) {
-                nodeX = Constants.Arm.TopX;
-                nodeY = Constants.Arm.TopShelfY;
-            } else {
-                nodeX = Constants.Arm.MidX;
-                nodeY = Constants.Arm.MidShelfY;
-            }
+        public static double nodeX;
+        public static double nodeY;
+        public static double dropHeight;
+
+        //LimeLight will determine this how u might ask? No clue 
+        public enum Node{
+            TOP_PEG,
+            MIDDLE_PEG,
+            TOP_SHELF,
+            MIDDLE_SHELF
         }
+
+        public static int Node nodeMode = 0;
+
+        //Sets the enum to int used to define which row it is
+        if(this.nodeMode == nodeMode.Node.TOP_PEG){
+            return this.nodeMode = 1;
+        }else if(this.nodeMode == nodeMode.Node.MIDDLE_PEG){
+            return this.nodeMode = 2;
+        }else if(this.nodeMode == nodeMode.Node.TOP_SHELF){
+            return this.nodeMode = 3;
+        }else if(this.nodeMode == nodeMode.Node.MIDDLE_SHELF){
+            return this.nodeMode = 4;
+        }else{
+            return this.nodeMode;
+        }
+
+        //TODO get Arm angle for the nodes, get arm distance from both nodes make the angle an distance variable/calculators
+        public getArmAngle(){
+
+            //Top Peg
+            if (this.nodeMode == 1){             //if (isTopRow) {
+                this.nodeX = Constants.Arm.TopX 
+                this.nodeY = Constants.Arm.TopPegY
+            }else if (){}
+        }
+        public getArmDistance(){}
+
+            //dropHeightPeg = Constants.Arm.dropHeightPeg; //dont have values yet
+            //} else {
+                //nodeX = Constants.Arm.MidX;
+                //nodeY = Constants.Arm.MidPegY;
+            //}
+        //} else {
+            //dropHeight = Constants.Arm.dropHeightShelf; //dont have values yet
+            //if (isTopRow) {
+                //nodeX = Constants.Arm.TopX;
+                //nodeY = Constants.Arm.TopShelfY;
+            //} else {
+                //nodeX = Constants.Arm.MidX;
+                //nodeY = Constants.Arm.MidShelfY;
+            //}
+        //}
 
         double distanceY = nodeY + dropHeight - Constants.Arm.armHeight;
         double distanceX = nodeX + distanceFromGrid + Constants.Arm.armDistanceFromFront;
