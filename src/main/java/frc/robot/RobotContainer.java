eb4c0b06391daa45e17f7904b8d15f801f3cbe6b
AidenMGaming <91638174+AidenMGaming@users.noreply.github.com>
2023-01-26 15:17:29 -0500

    Fixed button binding not pushing error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,27 +3,60 @@ package frc.robot;
 import frc.robot.Constants.OIConstants;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.ToggleIntake;
 
 public class RobotContainer {
 
     
     public Command m_intake;
+    public Command m_intakeReverse;
+    
+    public Joystick joy = new Joystick(0);
+    public JoystickButton a_button = new JoystickButton(joy, 1);
+    public JoystickButton b_button = new JoystickButton(joy, 2);
+    public JoystickButton x_button = new JoystickButton(joy, 3);
+    public JoystickButton y_button = new JoystickButton(joy, 4);
+    public JoystickButton l_bump = new JoystickButton(joy, 5);
+    public JoystickButton r_bump = new JoystickButton(joy, 6);
+    public JoystickButton left_menu = new JoystickButton(joy, 7);
+    public JoystickButton right_menu = new JoystickButton(joy, 8);
+    public JoystickButton left_stick = new JoystickButton(joy, 9);
+    public JoystickButton right_stick = new JoystickButton(joy, 10);
+  
+    public POVButton povUp = new POVButton(joy, 0, 0);
+    public POVButton povRight = new POVButton(joy, 90, 0);
+    public POVButton povDown = new POVButton(joy, 180, 0);
+    public POVButton povLeft = new POVButton(joy, 270, 0); 
+
     private Joystick m_joy = new Joystick(OIConstants.kDriverJoystickPort);
 
     public POVButton m_povUp = new POVButton(m_joy, 0, 0);
 
     public RobotContainer() {
 
-        configureButtonBindings();
+      this.m_intake = new ToggleIntake();
+      configureButtonBindings();
 
     };
 
     private void configureButtonBindings() {
-        
-       this.m_povUp.whenPressed(new InstantCommand(() -> Robot.m_swerveDriveSubsystem.zeroHeading()));
+       this.r_bump.onTrue(new InstantCommand() {
+        public void initialize() {
+          m_intake = new ToggleIntake();
+          m_intake.schedule();
+        }
+      });
 
+      this.r_bump.onFalse(new InstantCommand() {
+        public void initialize() {
+          if(m_intake.isScheduled()) {
+            m_intake.cancel();
+          }
+        }
+      });
     }   
 
     public double getJoystickRawAxis(int id) {
