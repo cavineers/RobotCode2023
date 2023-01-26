eab0f63497f5fb7fba96e3857aa6355583350e2f
AidenMGaming <91638174+AidenMGaming@users.noreply.github.com>
2023-01-26 15:10:56 -0500

    removed another push error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,16 +3,13 @@ package frc.robot;
 import frc.robot.Constants.OIConstants;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ArmControlCommand;
-import frc.robot.subsystems.Arm;
 
 public class RobotContainer {
 
     
-
+    public Command m_intake;
     private Joystick m_joy = new Joystick(OIConstants.kDriverJoystickPort);
 
     public POVButton m_povUp = new POVButton(m_joy, 0, 0);
