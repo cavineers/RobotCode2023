8f888da1f83d1df9722e23725ce6c9cde616ee9f
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-02-02 15:44:38 -0500

    Claw wants a test thing

---
+++ src/main/java/frc/robot/Vision.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Vision {
+    public Vision() {
+        // TODO Auto-generated constructor stub
+    
+    }
+
+    public int getObjectClass() {
+        return 0;
+    }
+}
