fb557d705efeb17e22806e59754274844fe4748c
Jonah Abellano <77077906+HxtCh0colate@users.noreply.github.com>
2023-02-02 13:34:12 -0500

    Updated claw code rq

--- src/main/java/frc/robot/commands/ClawToggle.java
+++ src/main/java/frc/robot/commands/ClawToggle.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
@@ -11,6 +12,7 @@ public class ClawToggle extends CommandBase {
     private boolean isDone = false;
     private boolean isHolding = false;
     private double m_timestamp;
+    private Joystick joy;
     
     public ClawToggle() {
         this.addRequirements(Robot.claw);
@@ -19,6 +21,8 @@ public class ClawToggle extends CommandBase {
     // Set Motor On / Off
     @Override
     public void initialize() {
+        this.m_timestamp = Timer.getFPATimestamp();
+        
         if (Robot.claw.getClawMotorState() == Claw.ClawMotorState.OFF) {
             Robot.claw.setMotorState(Claw.ClawMotorState.ON);
             this.isDone = false;
