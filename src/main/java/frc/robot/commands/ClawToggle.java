1e1702403e4e45af788b2e823e6fdf8042749ab9
Jonah Abellano <77077906+HxtCh0colate@users.noreply.github.com>
2023-02-02 11:53:18 -0500

    Claw code test ver. 1

--- src/main/java/frc/robot/commands/ClawToggle.java
+++ src/main/java/frc/robot/commands/ClawToggle.java
@@ -1,11 +1,9 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.Claw;
-import frc.robot.Constants;
 
 
 public class ClawToggle extends CommandBase {
@@ -13,7 +11,6 @@ public class ClawToggle extends CommandBase {
     private boolean isFinished = false;
     private boolean isHolding = false;
     private double m_timestamp;
-    private Joystick joy;
     
     public ClawToggle() {
         this.addRequirements(Robot.claw);
@@ -22,15 +19,12 @@ public class ClawToggle extends CommandBase {
     // Set Motor On / Off
     @Override
     public void initialize() {
-        this.m_timestamp = Timer.getFPGATimestamp();
-
         if (Robot.claw.getClawMotorState() == Claw.ClawMotorState.OFF) {
             Robot.claw.setMotorState(Claw.ClawMotorState.ON);
             this.isFinished = false;
-            this.isHolding = true;
         } else {
             Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
-            this.isHolding = false;
+            this.isFinished = true;
         }
     }
 
@@ -47,6 +41,10 @@ public class ClawToggle extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return true;
+        if (Timer.getFPGATimestamp() - this.m_timestamp >= 0.4 && Robot.m_robotContainer.joy.getRawButton(2)) {
+            this.isDone = true;
+        }
+
+        return this.isDone;
     }
 } 
