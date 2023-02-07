d48429aa1c498ed82dbb5911a6331114239ff79c
AidenMGaming <91638174+AidenMGaming@users.noreply.github.com>
2023-02-07 12:45:33 -0500

    Renamed Raise/Lower to Undeploy/Deploy & added encoder values

--- src/main/java/frc/robot/commands/ToggleDeployIntake.java
+++ src/main/java/frc/robot/commands/ToggleDeployIntake.java
@@ -1,38 +1,34 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.Intake;
 
-public class ToggleRaiseIntake extends CommandBase {
+public class ToggleDeployIntake extends CommandBase {
 
-    boolean isFinished = false;
-
-    public ToggleRaiseIntake() {
+    private boolean isFinished = false;
+    
+    public ToggleDeployIntake() {
         this.addRequirements(Robot.intake);
     }
 
-    // Set Motor State to ON / OFF
     @Override
     public void initialize() {}
 
     @Override
     public void execute() {
-        SmartDashboard.putBoolean("Limit Switch", Robot.intake.getIntakeSwitch());
         if (Robot.intake.getIntakeDropMotorState() == Intake.IntakeDropMotorState.OFF) {
-            Robot.intake.setIntakeDropMotorState(Intake.IntakeDropMotorState.RAISE);
-            Robot.intake.setIntakeMotorState(Intake.IntakeMotorState.OFF);
-        }
-        if(Robot.intake.getIntakeSwitch() == true) {
+            Robot.intake.setIntakeDropMotorState(Intake.IntakeDropMotorState.DEPLOY);
+            Robot.intake.setIntakeMotorState(Intake.IntakeMotorState.ON);
+        } if (Robot.intake.getIntakeDropMotor().getEncoder().getPosition() <= Constants.Intake.RevolutionsToLower) {
             Robot.intake.setIntakeDropMotorState(Intake.IntakeDropMotorState.OFF);
             isFinished = true;
         }
     }
 
     @Override
-    public void end(boolean interrupted) {
-    }
+    public void end(boolean interrupted) {}
 
     @Override
     public boolean isFinished() {
