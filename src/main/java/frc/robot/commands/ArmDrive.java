fce4638554a1dd47d9bbbb42e1fc42d19b93ed18
BechtelLo25 <91638125+BechtelLo25@users.noreply.github.com>
2023-02-10 11:17:11 -0500

    start arm manual overide

---
+++ src/main/java/frc/robot/commands/ArmDrive.java
@@ -0,0 +1,52 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.Arm;
+
+public class ArmDrive extends CommandBase {
+    
+    private Joystick joy;
+    private boolean isDone = false;
+    private double m_timestamp;
+    private RobotContainer rc;
+
+    public ArmDrive(RobotContainer container, Joystick joy) {
+        this.joy = joy;
+        this.rc = container;
+        this.addRequirements(Robot.arm);
+    }
+
+    // Set Motor State to ON / OFF
+    @Override
+    public void initialize() {
+        if (Robot.arm.getArmExtensionMotorState() == Arm.ArmExtensionMotorState.OFF) {
+            Robot.arm.setArmExtensionMotorState(Arm.ArmExtensionMotorState.ON);
+            this.isDone = false;
+        } else {
+            Robot.arm.setArmExtensionMotorState(Arm.ArmExtensionMotorState.OFF);
+            this.isDone = true;
+        }
+    }
+
+    @Override
+    public void execute() {
+        Robot.arm.setArmExtensionMotorState(Arm.ArmExtensionMotorState.ON);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Robot.arm.setArmExtensionMotorState(Arm.ArmExtensionMotorState.OFF);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (Timer.getFPGATimestamp() - this.m_timestamp >= 0 && Robot.m_robotContainer.joy.getRawButton(0)) {
+            this.isDone = true;
+        }
+
+        return this.isDone;
+    }
+}
