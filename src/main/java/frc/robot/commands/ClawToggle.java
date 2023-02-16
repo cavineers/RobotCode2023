d5c5f5a0edce59169e876992fc035e7dfaec2e19
Jonah Abellano <77077906+HxtCh0colate@users.noreply.github.com>
2023-02-16 15:21:00 -0500

    3rd change (claw toggle new code)

--- src/main/java/frc/robot/commands/ClawToggle.java
+++ src/main/java/frc/robot/commands/ClawToggle.java
@@ -1,15 +1,16 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.Claw;
 import frc.robot.Vision;
+import frc.robot.Constants;
 
 
 public class ClawToggle extends CommandBase {
-    
+   
+    private boolean isDone = false;
+ 
     public ClawToggle() {
         this.addRequirements(Robot.claw);
     }
@@ -17,24 +18,38 @@ public class ClawToggle extends CommandBase {
     // Set Motor On / Off
     @Override
     public void initialize() {
+    
     }
 
+    // Motor will move to set position based on vision. Whether Cube || Cone
     @Override
     public void execute() {
-        if (Robot.claw.getClawMotorState() == Claw.ClawMotorState.OFF) {
-            Robot.claw.setMotorState(Claw.ClawMotorState.ON);
-        } else {
-            Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
+        if (Vision.CUBE = true) {
+            if(Robot.claw.m_ClawMotor.getEncoder().getPosition() <= Constants.Claw.RevolutionsToCube) {
+                Robot.claw.setMotorState(Claw.ClawMotorState.ON);
+            } else if(Robot.claw.m_ClawMotor.getEncoder().getPosition() >= Constants.Claw.RevolutionsToCube) {
+                Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
+                Robot.claw.m_ClawMotor.getEncoder().setPosition(0);
+                isDone = true;
+            } 
+        } else if (Vision.CONE = true) {
+            if(Robot.claw.m_ClawMotor.getEncoder().getPosition() <= Constants.Claw.RevolutionsToCone) {
+                Robot.claw.setMotorState(Claw.ClawMotorState.ON);
+            } else if(Robot.claw.m_ClawMotor.getEncoder().getPosition() >= Constants.Claw.RevolutionsToCone) {
+                Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
+                Robot.claw.m_ClawMotor.getEncoder().setPosition(0);
+                isDone = true;
+            } 
         }
     }
 
-
     @Override 
     public void end(boolean interrupted) {
+        Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return isDone;
     }
 } 
