314e3cbe9f47f475b248cb44d1df0b4ad41ef999
Jonah Abellano <77077906+HxtCh0colate@users.noreply.github.com>
2023-02-16 15:22:49 -0500

    vision test code update 2/16

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,12 +1,25 @@
 package frc.robot;
 
 public class Vision {
+    public static boolean CUBE = false;
+    public static boolean CONE = false;
+
     public Vision() {
         // TODO Auto-generated constructor stub
     
     }
 
     public int getObjectClass() {
-        return 0;
+        return (int) (Math.random() * 2);
+    }
+
+    public void defineObject() {
+    if(this.getObjectClass() == 0) {
+        CUBE = true;
+        CONE = false;
+    } else if (this.getObjectClass() == 1) {
+        CONE = true;
+        CUBE = false;
+    }
     }
 }
