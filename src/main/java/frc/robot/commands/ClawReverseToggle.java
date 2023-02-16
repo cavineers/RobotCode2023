0d7d368014e531f108df8f0e1ada5739aebe7688
Jonah Abellano <77077906+HxtCh0colate@users.noreply.github.com>
2023-02-16 15:19:23 -0500

    2nd change (update to real claw)

--- src/main/java/frc/robot/commands/ClawReverseToggle.java
+++ src/main/java/frc/robot/commands/ClawReverseToggle.java
@@ -1,36 +1,45 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.Claw;
+import frc.robot.Vision;
+import frc.robot.Constants;
+
 
 public class ClawReverseToggle extends CommandBase {
-
-    private boolean isFinished = false;
-    private boolean holding = false;
+    
+    private boolean isDone = false;
+    private double m_timestamp;
 
     public ClawReverseToggle() {
         this.addRequirements(Robot.claw);
     }
 
-    // Set Motor State to REVERSED / OFF
+    // Set Motor On / Off
     @Override
     public void initialize() {
-        if (Robot.claw.getClawMotorState() == Claw.ClawMotorState.OFF) {
-            Robot.claw.setMotorState(Claw.ClawMotorState.REVERSE);
-        } else {
-            Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
-        }
     }
 
     @Override
-    public void execute() {}
+    public void execute() {
+        if (Robot.claw.getClawMotorState() == Claw.ClawMotorState.OFF) {
+            Robot.claw.setMotorState(Claw.ClawMotorState.REVERSE);
+        } else if (Robot.claw.m_ClawMotor.getEncoder().getPosition() <= Constants.Claw.RevolutionsToCone){
+            Robot.claw.setMotorState(Claw.ClawMotorState.OFF);
+            isDone = true;
+        }                
+    }
 
-    @Override
-    public void end(boolean interrupted) {}
+
+    @Override 
+    public void end(boolean interrupted) {
+    }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return isDone;
     }
-}
+} 
