d56e7b63008bf47f1703cee307d0af2e1a1bc217
Jonah Abellano <77077906+HxtCh0colate@users.noreply.github.com>
2023-02-16 15:40:28 -0500

    Create Claw.java

---
+++ src/main/java/frc/robot/Claw.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Claw extends SubsystemBase {
+
+    public enum ClawMotorState {
+        ON,
+        OFF,
+        REVERSE,
+        CUBEHOLD,
+        CONEHOLD
+    }
+
+    boolean isOn;
+    boolean opening;
+    boolean closing;
+    boolean holdingCube;
+    boolean holdingCone;
+    private static boolean isClosed = false;
+
+    public CANSparkMax m_ClawMotor = new CANSparkMax(Constants.Claw.ClawID, MotorType.kBrushless);
+    public ClawMotorState m_ClawMotorState = ClawMotorState.OFF;
+
+    public ClawMotorState getClawMotorState() {
+        return this.m_ClawMotorState;
+    }
+
+    public void setMotorState(ClawMotorState state) {
+        // set current state
+        this.m_ClawMotorState = state;
+        // set motor state
+        switch (state) {
+            case ON:
+                // turns on motor
+                this.m_ClawMotor.set(Constants.Claw.ClawSpeed);
+                break;
+            case OFF:
+                // turns off motor
+                this.m_ClawMotor.set(0.0);
+                break;
+            case REVERSE:
+                // reverses intake
+                this.m_ClawMotor.set(Constants.Claw.ClawSpeedRev);
+                break;
+
+            default: 
+                this.setMotorState(ClawMotorState.OFF);
+        }
+    }
+
+    public CANSparkMax getClawMotor() {
+        return this.m_ClawMotor;
+    }
+}
