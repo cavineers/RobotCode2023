fd853c3fe6ea177140b45413b1c8d2d84a7d6c0b
BechtelLo25 <91638125+BechtelLo25@users.noreply.github.com>
2023-03-04 13:25:52 -0500

    Revert "Merge main with arm"
    This reverts commit 677f79a5aace79153ac7c5fcbf1db47eb9785cac, reversing
    changes made to c797e86d69f706855aebdaa06dffac9dc9d51b0d.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,10 +7,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.DriverStation;
-
 import frc.robot.subsystems.Arm;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Claw;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -25,8 +22,6 @@ public class Robot extends TimedRobot {
 
   //Subsystems
   public static Arm arm;
-  public static Intake intake;
-  public static Claw claw;
 
 
   //Navx
@@ -41,10 +36,8 @@ public class Robot extends TimedRobot {
   public Robot() {
 
     //Subsystems
+    
     arm = new Arm();
-    intake = new Intake();
-    claw = new Claw();
-
     //Container
     m_robotContainer = new RobotContainer();
 
