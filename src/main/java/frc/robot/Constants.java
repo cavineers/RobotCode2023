afdb5bf51e589f0aae30a606376aee30110257c0
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-03-13 17:25:01 -0400

    Update Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -191,9 +191,9 @@ public final class Constants {
     public static final class PresetTranslations {
         //IN METERS needs to be adjusted 
 
-        public static final Translation2d kLeftPosition = new Translation2d(1.24, Units.inchesToMeters(22.15)); 
-        public static final Translation2d kShelfPosition = new Translation2d(1.24, 0); 
-        public static final Translation2d kRightPosition = new Translation2d(1.24, Units.inchesToMeters(-22.15)); 
+        public static final Translation2d kLeftPosition = new Translation2d(Units.inchesToMeters(50), Units.inchesToMeters(22.15)); 
+        public static final Translation2d kShelfPosition = new Translation2d(Units.inchesToMeters(50), 0); 
+        public static final Translation2d kRightPosition = new Translation2d(Units.inchesToMeters(50), Units.inchesToMeters(-22.15)); 
 
         public static final Translation2d kSubstationPosition = new Translation2d(0.5, 0); 
 
