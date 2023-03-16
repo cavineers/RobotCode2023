04bb2841764901c43a3f09fddff7217bc44520c0
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-03-16 15:10:37 -0400

    Update SwerveModule.java

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -104,7 +104,7 @@ public class SwerveModule {
             turningMotor.setIdleMode(IdleMode.kBrake);
         }
         else{
-            turningMotor.setIdleMode(IdleMode.kBrake);
+            turningMotor.setIdleMode(IdleMode.kCoast);
         }
     }
 
