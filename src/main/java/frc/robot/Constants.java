7efafb6e882996905753d052065f777dea906b2c
Robert Snell <91638194+roberts1y@users.noreply.github.com>
2023-03-23 09:21:17 -0400

    Finish double sub command - logan from big robs laptop

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -168,7 +168,7 @@ public final class Constants {
         public static double TopNodeShelfExtensionRotations = 130;
         public static double TopNodePegAngleRotations = 19.3;
         public static double TopNodePegExtensionRotations = 130;
-        public static double DoubleSubStationAngleRotations = 20;
+        public static double DoubleSubStationAngleRotations = 16;
         public static double DoubleSubStationExtensionRotations = 5;
     // Encoder deadzones
      public static double ExtensionEncoderDeadzone = 2.5;
